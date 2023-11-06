24ca79663c476f0b4207615aa30195fb1f547d34
madyfleexGT <89738692+madyfleex@users.noreply.github.com>
2023-11-06 03:44:12 -0400

    Update http.py

--- src/modules/libs/http.py
+++ src/modules/libs/http.py
@@ -1,5 +1,4 @@
-import orjson as json
-import httpx
+from urllib import request
 
 class RequestError(Exception):
     """
@@ -9,7 +8,7 @@ class RequestError(Exception):
 
 class Response:
     """
-    Represents the Response object given after an HTTPs request.
+    Represents the Response object given after an HTTP request.
     """
     def __init__(self, content, headers):
         self.status_code = None
@@ -24,52 +23,54 @@ class Response:
         return json.loads(self.text)
 
 class Request:
-    def __init__(self, method: str, url: str, headers: dict, data: dict, proxy=None, timeout=None):
+    def __init__(self, method: str, headers: dict, url: str, data: dict, proxy=None, timeout=None):
         self.method = method
         self.url = url
-        self.headers = headers
         self.data = data
+        self.headers = headers
         self.proxy = proxy
         self.timeout = timeout
 
     def get_response(self):
-        if self.proxy:
-            client = httpx.Client(proxies='http://' + self.proxy, timeout=self.timeout)
-        else:
-            client = httpx.Client(timeout=self.timeout)
-        response = None
-
+        headers = self.headers
         if self.method == "GET":
-            response = client.get(self.url, headers=self.headers)
+            req = request.Request(self.url, headers=headers, method=self.method)
         else:
-            response = client.request(self.method, self.url, json=self.data, headers=self.headers)
+            json_data = json.dumps(self.data)
+            req = request.Request(self.url, data=json_data, headers=headers, method=self.method)
+
+        if self.proxy:
+            proxy_handler = request.ProxyHandler({"https": "http://" + self.proxy})
+            opener = request.build_opener(proxy_handler)
+            response = opener.open(req, timeout=self.timeout)
+        else:
+            response = request.urlopen(req, timeout=self.timeout)
 
         content = response.read()
-        client.close()
         return Response(content, response.headers)
 
-class HttpReq:
+class SocketReq:
     """
     Represents an HTTP/S client.
     """
-    def send_request(self, method: str, url: str, headers=None, data=None, proxy=None, timeout=None) -> Response:
+    def send_request(self, method: str, url: str, data=None, proxy=None, timeout=None, headers=None) -> Response:
         headers = headers or {}
         try:
-           return Request(method, url, headers, data, proxy=proxy, timeout=timeout).get_response()
+            return Request(method, headers, url, data, proxy=proxy, timeout=timeout).get_response()
         except:
             pass
 
-    def get(self, url: str, headers=None, proxy=None, timeout=None) -> Response:
-        return self.send_request(method="GET", url=url, headers=headers, proxy=proxy, timeout=timeout)
+    def get(self, url: str, proxy=None, timeout=None, headers=None) -> Response:
+        return self.send_request(method="GET", url=url, proxy=proxy, timeout=timeout, headers=headers)
 
-    def post(self, url: str, data: dict, headers=None, proxy=None, timeout=None):
-        return self.send_request(method="POST", url=url, headers=headers, data=data, proxy=proxy, timeout=timeout)
+    def post(self, url: str, data: dict, proxy=None, timeout=None, headers=None):
+        return self.send_request(method="POST", url=url, data=data, proxy=proxy, timeout=timeout, headers=headers)
 
-    def put(self, url: str, data: dict, headers=None, proxy=None, timeout=None):
-        return self.send_request(method="PUT", url=url, headers=headers, data=data, proxy=proxy, timeout=timeout)
+    def put(self, url: str, data: dict, proxy=None, timeout=None, headers=None):
+        return self.send_request(method="PUT", url=url, data=data, proxy=proxy, timeout=timeout, headers=headers)
 
-    def patch(self, url: str, data: dict, headers=None, proxy=None, timeout=None):
-        return self.send_request(method="PATCH", url=url, headers=headers, data=data, proxy=proxy, timeout=timeout)
+    def patch(self, url: str, data: dict, proxy=None, timeout=None, headers=None):
+        return self.send_request(method="PATCH", url=url, data=data, proxy=proxy, timeout=timeout, headers=headers)
 
-    def delete(self, url: str, data: dict, headers=None, proxy=None, timeout=None):
-        return self.send_request(method="DELETE", url=url, headers=headers, data=data, proxy=proxy, timeout=timeout)
+    def delete(self, url: str, data: dict, proxy=None, timeout=None, headers=None):
+        return self.send_request(method="DELETE", url=url, data=data, proxy=proxy, timeout=timeout, headers=headers)
